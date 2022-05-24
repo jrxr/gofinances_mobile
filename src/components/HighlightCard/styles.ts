30bc56a5c885101c54a8a1ff535478f7f800d6f5
Agacy junior <juniormartinssom@gmail.com>
2022-05-24 16:53:12 -0300

    Acessando propriedades no Styled Components

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
@@ -9,28 +9,40 @@ export const Container = styled.View`
 
   padding: 19px 23px;
   padding-bottom: ${RFValue(42)}px;
+  margin-right: 16px;
+
+  height: 300px;
 `;
 
 export const Header = styled.View`
-
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
 
+  color: ${({ theme }) => theme.colors.text_dark};
 `;
 
 export const Icon = styled(Feather)`
-
+  font-size: ${RFValue(40)}px;
 `;
 
-export const Footer = styled.View`
-
-`;
+export const Footer = styled.View``;
 
 export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(32)}px;
 
+  color: ${({ theme }) => theme.colors.text_dark};
+  margin-top: 38px;
 `;
 
 export const LastTransaction = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(12)}px;
 
+  color: ${({ theme }) => theme.colors.text};
 `;
