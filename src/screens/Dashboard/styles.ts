8d060be0cebb291ce603020018a760d7424540f4
Agacy junior <juniormartinssom@gmail.com>
2022-05-24 16:24:38 -0300

    Criação dos cartões de Destaque

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
